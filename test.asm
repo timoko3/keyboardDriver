431f6c3da44de49ee30d3cf79f5cef0570ecab81
timoko3 <freepower27@gmail.com>
2026-02-21 18:17:46 +0300

    ready basic version of drawing regs

--- test.asm
+++ test.asm
@@ -4,11 +4,27 @@
 org 100h
 
 Start: 
-    mov ax, 0d
+    mov bx, 0d
+    mov cx, 3d
 
     startCycle:
-    cmp ax, 0d
+    cmp bx, 60000d
     je endCycle
+        inc bx
         jmp startCycle
     endCycle:
+
+    mov bx, 0d
+
+    startCycle2:
+    cmp bx, 60000d
+    je endCycle2
+        inc bx
+        jmp startCycle2
+    endCycle2:
+
+    inc cx
+
+    mov ax, 04C00h
+    int 21h
 end Start
